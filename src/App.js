c7a63f608a4c068b93334cb1cec5cf3997017997
unknown <aguerofranco2002@gmail.com>
2021-06-09 17:43:37 -0300

    Router de NavBar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import {Route} from 'react-router-dom';
 
-import Home from './Components/home'
+import Home from './Components/home';
+import NavBar from './Components/NavBar/navBar';
 
 function App() {
   return (
 
     <div >
       <Route exact path='/' component={Home} />
+      <Route path='/navBar' component={NavBar} />
     </div>
 
   );
